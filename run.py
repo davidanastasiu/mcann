f95d9246f07d9837fb80af72e14af70bb87bfc91
YLi2021 <77151116+YLi2021@users.noreply.github.com>
2024-07-01 17:27:13 -0400

    Add files via upload

--- run.py
+++ run.py
@@ -28,7 +28,7 @@ class Options():
         self.parser.add_argument('--os_v', type=int, default=5, help='oversampling frequency')
         self.parser.add_argument('--seq_weight', type=float, default=0.3, help='sequence cluster weight')        
         self.parser.add_argument('--batchsize', type=int, default=48, help='batch size of train data')
-        self.parser.add_argument('--epochs', type=int, default=50, help='train epochs')
+        self.parser.add_argument('--epochs', type=int, default=1, help='train epochs')
         self.parser.add_argument('--learning_rate', type=float, default=0.001, help='learning rate')
         self.parser.add_argument('--lradj', type=str, default='type4', help='learning rate adjustment policy')
         
@@ -205,18 +205,13 @@ if __name__ == '__main__':
     ds = DS(opt, trainX, R_X)
 
     # model training
-    model = MCANN(opt,ds)
+    model = DAN(opt,ds)
     model.train()
 
     # Inferencing, saving the result to Inference_dir      
     ds.refresh_dataset(trainX, R_X)
     model.model_load()      
-    Inference_result = model.inference()
-
-    # Computing RMSE and MAPE
-    rmse, mape = model.compute_metrics(Inference_result)
-    print("RMSE: ", rmse)
-    print("MAPE: ", mape)   
+    Inference_result = model.inference() 
     
     # Save the model
     expr_dir = os.path.join(opt.outf, opt.name, 'train')
