22cae6d55a3fa929c314ff12baf96d219ab44270
YLi2021 <77151116+YLi2021@users.noreply.github.com>
2024-07-01 17:26:39 -0400

    Add files via upload

--- utils/metric.py
+++ utils/metric.py
@@ -36,6 +36,7 @@ def metric(model, pred, true):
     mape = MAPE(pred, true)
 
     return mae, mse, rmse, mape
+
 def metric_g(name, pre, gt):
     pre = np.array(pre)
     gt = np.array(gt)
@@ -59,3 +60,43 @@ def metric_g(name, pre, gt):
     lll.append(l3)
 
     return lll
+
+def metric_rolling1(name, pre, gt, rm=16):
+    pre = np.array(pre)
+    gt = np.array(gt)
+    ll = int(len(pre)/72)
+    mae_all = []
+    mse_all = []
+    rmse_all = []
+    mape_all = []
+    l2 = []
+    l3 = []
+    lll=[]
+    for i in range(ll):
+        mae, mse, rmse, mape = metric(name, pre[i*72:(i*72+rm)], gt[i*72:(i*72+rm)])
+        rmse_all.append(rmse)
+        mape_all.append(mape)
+    l2.append(np.around(np.mean(np.array(rmse_all)),2))
+    l3.append(np.around(np.mean(np.array(mape_all)),3))
+    lll.append(l2)
+    lll.append(l3)
+    print("For rolling prediction: ", name)
+    print("RMSE: ", np.array(lll[0][0]))
+    print("MAPE: ", np.array(lll[1][0]))
+    return lll
+
+def metric_rolling(name, pre, gt, rm=16):
+    pre = np.array(pre)
+    gt = np.array(gt)
+    ll = int(len(pre)/72)
+    pre_all = []
+    gt_all = []
+    for i in range(ll):
+        pre_all.extend(pre[i*72:(i*72+rm)])
+        gt_all.extend(gt[i*72:(i*72+rm)])
+    _, _, rmse, mape = metric(name, np.array(pre_all), np.array(gt_all))
+
+    print("For rolling prediction: ", name)
+    print("RMSE: ", rmse)
+    print("MAPE: ", mape)
+    return rmse, mape
